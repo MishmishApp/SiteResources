7a883ab66b82cc39456ae6eccdfb0a3ab1787547
Niro <20151466+nirdil@users.noreply.github.com>
2025-01-30 13:15:48 +0200

    Update resourcemap.js

--- resourcemap.js
+++ resourcemap.js
@@ -243,7 +243,7 @@ var resources = {
       title: 'Homemade Chocolate Recipe - Healthy Recipes Blog',
       subtitle: 'This ultra-rich and decadent homemade chocolate is made with cocoa powder, coconut oil, honey, and chopped nuts.',
       url: 'https://healthyrecipesblogs.com/homemade-chocolate/',
-      image: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/healthyrecipesblog2.png',
+      image: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/healthyrecipesblog3.png',
       snapshot: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/healthyrecipesblog.png'
     },
     joyfoodsunshine:  {
