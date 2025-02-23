99ef1caaddcf5c28076bf725a40eb98bd145dc79
Niro <20151466+nirdil@users.noreply.github.com>
2025-02-23 09:45:23 +0200

    Update resourcemap.js

--- resourcemap.js
+++ resourcemap.js
@@ -10,7 +10,8 @@ var resources = {
     product6: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/product6.webp',
     product7: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/product7_2.webp',
     product8: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/product8.webp',
-    product9: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/product9.webp'
+    product9: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/product9.webp',
+    airlines: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/airlinessprite.webp'
   },
   icons: {
     options: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" /></svg>',
