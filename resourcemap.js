d1ed567e05c9351ee43ac5c42b4b02aeac61776b
Niro <20151466+nirdil@users.noreply.github.com>
2025-04-06 09:11:16 +0300

    Update resourcemap.js

--- resourcemap.js
+++ resourcemap.js
@@ -20,6 +20,7 @@ var resources = {
     restaurants: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/restaurants_sprite.webp',
     vehiclesbg: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/rome_holo_map.webp',
     vehicles: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/vehicles_sprite.webp',
+    cookiejar: 'https://cdn.jsdelivr.net/gh/MishmishApp/SiteResources/images/cookiejar.png'
   },
   icons: {
     options: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" /></svg>',
